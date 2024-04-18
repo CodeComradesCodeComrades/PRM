c30be0b0225657e6c7c2a96fe88a27c714b6f9d6
Florian Reichelt | Fllooo <mail@florian-reichelt.de>
2024-04-18 09:18:17 +0200

    hotfix(remove missing import for test controller)

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,5 +1,4 @@
 import { AuthController } from 'src/controllers/auth.controller';
-import { TestController } from 'src/controllers/test.controller';
 import { UserController } from 'src/controllers/user.controller';
 
-export const controllers = [UserController, AuthController, TestController];
+export const controllers = [UserController, AuthController];
