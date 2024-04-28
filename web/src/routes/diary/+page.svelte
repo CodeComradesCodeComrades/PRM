030bf3bab79d863d59913344f631769058c3fccb
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-28 03:52:42 +0200

    Create UserPageLayout Component and Diary Page

--- web/src/routes/diary/+page.svelte
+++ web/src/routes/diary/+page.svelte
@@ -1,7 +1,24 @@
 <script>
-    import Navbar from "$lib/Navbar.svelte";
-    import Sidebar from "$lib/Sidebar.svelte";
+  import UserPageLayout from "$lib/UserPageLayout.svelte";
 </script>
 
-<Navbar />
-<Sidebar />
+<UserPageLayout>
+  <div>
+    <div class="diary-entry">
+      <p class="roboto">28. April 2024</p>
+    </div>
+  </div>
+</UserPageLayout>
+
+<style>
+  .diary-entry {
+  }
+
+  .roboto {
+    color: white;
+    font-size: 18px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style>
