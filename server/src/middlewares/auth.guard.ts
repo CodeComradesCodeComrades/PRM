03e994acc933224cf88e1eb0efe45d089395d87f
Florian Reichelt | Fllooo <mail@florian-reichelt.de>
2024-05-06 17:18:12 +0200

    asdasd

--- server/src/middlewares/auth.guard.ts
+++ server/src/middlewares/auth.guard.ts
@@ -85,7 +85,7 @@ export const Auth = createParamDecorator((data, context: ExecutionContext): Auth
     return context.switchToHttp().getRequest<{ user: AuthDto }>().user;
 });
 
-export const AdminRoute = (value = true) => SetMetadata(Metadata.ADMIN_ROUTE, value);
+export const            AdminRoute = (value = true) => SetMetadata(Metadata.ADMIN_ROUTE, value);
 
 export const PublicRoute = () =>
     applyDecorators(SetMetadata(Metadata.AUTH_ROUTE, false), ApiSecurity(Metadata.PUBLIC_SECURITY));
